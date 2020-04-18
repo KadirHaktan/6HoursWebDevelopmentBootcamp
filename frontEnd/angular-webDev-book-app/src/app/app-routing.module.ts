9ae2408fb5bb52c2c91624140ae3dfc117076f1a
Hacktin <isues41@gmail.com>
2020-04-19 01:00:37 +0300

    app component html has been edited and routing module

--- frontEnd/angular-webDev-book-app/src/app/app-routing.module.ts
+++ frontEnd/angular-webDev-book-app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BookComponent} from './components/book/book.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path:"books",component:BookComponent},
+   {path:"",component:BookComponent,pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
