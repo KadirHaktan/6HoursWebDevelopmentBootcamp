03fcf98840877549c7e722c5b66bad348e2688bd
Hacktin <isues41@gmail.com>
2020-04-19 21:31:49 +0300

    Some commands has been written

--- backEnd/WebDevBackEnd/DataAccess/Abstract/IBookDal.cs
+++ backEnd/WebDevBackEnd/DataAccess/Abstract/IBookDal.cs
@@ -7,4 +7,10 @@ namespace DataAccess.Abstract
     public interface IBookDal:IEntityRepository<Book>
     {
     }
+    //IBookDal kendisinden implemente edecek olan bir Ef için ya da Dapper vs ... farklı ORM sistemlerin dal sınıfların
+    //implemente edildi interface'nin ta kendisidir.Dikkat edin yine bir interface'den de implemente edilmiş o da
+    //genel CRUD veritabanı işlemlerin imzalandığı ve Repository tasarım deseni uygulayan interface'nin ta kendisidir.
+    //Repository tasarım deseni burada aslında uygulanıyor diyebiliriz ki IBookDal interface'i içinde tekrar tekrar
+    //Add ya da Delete operasyonunu burada imzalı olarak tanımlama ithiyacımız ortadan kaldırıyor.Çünkü bu işlemler
+    //IEntityRepository interface'i içinde merkezileştirerek imzalanmıştır.
 }
--- backEnd/WebDevBackEnd/DataAccess/Concrete/EntityFramework/EfBookDal.cs
+++ backEnd/WebDevBackEnd/DataAccess/Concrete/EntityFramework/EfBookDal.cs
@@ -7,6 +7,27 @@ using Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
+
+    //EfBookDal dikkat edin EfEntityRepositoryBase'den kalıtım almış
+    //kalıtım aldığı için artık EntityFramework'deki add delete tarzı CRUD operasyonların Ef kodlarını
+    //tekrar tekrar bu sınıfın içinde yazma ithiyacını ortadan kaldırıyor ve aynı zamanda EfBookDal
+    //IBookDal'dan implemente edilmiş.
+
+    //IBookDal çünkü tüm ORM sistemleri imzalayan ve destekleyen genel bir referans tip
+    //Bu genel referans olabilmesi sayesinde bir üst katmanda yani iş katmanı sınıfının herhangi bir ORM
+    //sınıfına bağımlı kalmasını önlemiş olacak.
+
+    //IBookDal interface'i bildiğiniz gibi aynı zamanda IEntityRepository interface'inden besleniyor ki
+    //aynı şekilde burada EfBookDal'dan kendi repository'si olan EfEntityRepositoryBase<Book,BookStoreContext>'den besleniyor ki
+    //Beslenmeleri sayesinde de genel CRUD operasyonları tek bir merkezden yönetebiliyor ve ilgili operasyonları kullanabiliyorlar.
+
+    //Bir detay daha EfEntityRepositoryBase'de IEntityRepository'den implemente edildi.Başka bir ORM için de bir repository
+    //yazılacağı zaman Ef ellenmeden başka bir class tanımlanıp yeniden IEntityRepository'den implemente edilebilmekte
+    //IEntityRepository'de işte tam burda genel CRUD operasyonların imzası görevi görüyor ve başka sınıflar bu interface'den
+    //implemente edildikçe sadece operasyonların kod syntax gereği olması gereken değişiklikler yapılabilmekte ve mevcut sistemler de ellenmemekte
+
+
+
     public class EfBookDal:EfEntityRepositoryBase<Book,BookStoreContext>,IBookDal
     {
     }
